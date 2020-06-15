ac3ce0d050bcdd8010325ae9142623fef62cd328
Shiroze <lord_paleo@yahoo.com>
2020-06-15 10:44:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ export default class TextInputMask extends Component {
     }
   }
 
-  static getDerivedStateFromProps(nextProps) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
     if (nextProps.mask && (this.props.value !== nextProps.value)) {
       mask(this.props.mask, '' + nextProps.value, text =>
       this.input && this.input.setNativeProps({ text })
